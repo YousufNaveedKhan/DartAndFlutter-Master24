d1d087231083892d245a7b95ea15244c37a329d8
YousufNaveedKhan <yk033127@gmail.com>
2024-10-23 09:21:01 -0700

    DartAndFlutter-Master24 Update #7

--- All-Material/Assignments/FlutterSecondary/a1_quran_api/lib/main.dart
+++ All-Material/Assignments/FlutterSecondary/a1_quran_api/lib/main.dart
@@ -1,8 +1,8 @@
-import 'dart:async';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:http/http.dart' as http;
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -11,140 +11,199 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
+      title: 'Quran',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Color(0xff023E73)),
         useMaterial3: true,
       ),
-      home: const Splash(),
+      home: const SplashScr(),
     );
   }
 }
 
-class Splash extends StatefulWidget {
-  const Splash({super.key});
+class SplashScr extends StatefulWidget {
+  const SplashScr({super.key});
 
   @override
-  State<Splash> createState() => _SplashState();
+  State<SplashScr> createState() => _SplashScrState();
 }
 
-class _SplashState extends State<Splash> {
-  @override
+class _SplashScrState extends State<SplashScr> {
   void initState() {
     // TODO: implement initState
-    Timer(
-      Duration(seconds: 3), 
-    () => {
-      Navigator.pushReplacement(
-        context, 
-        MaterialPageRoute(
-          builder: (context) => Surahs()
-          ))
-    });
     super.initState();
+
+    Timer(Duration(seconds: 3), (() {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => SurahIndex()));
+    }));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        child: Image.asset("assets/images/quran.png")
+        body: Center(
+            child: Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+            colors: [
+              Color(0xff010D26),
+              Color(0xff023E73),
+            ]),
       ),
-    );
+      child: const Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: Text(
+            "Quran ",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: "jameel",
+                fontSize: 30,
+                color: Color(0XFFF2F2F2)),
+          )),
+          Center(
+              child: Text(
+            "القرآن",
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontFamily: "jameel",
+                fontSize: 30,
+                color: Color(0XFFF2F2F2)),
+          )),
+        ],
+      ),
+    ))
+        // backgroundColor: Color(0XFFD9D9D9)
+        );
   }
 }
 
-class Surahs extends StatefulWidget {
-  const Surahs({super.key});
+class SurahIndex extends StatefulWidget {
+  const SurahIndex({super.key});
 
   @override
-  State<Surahs> createState() => _SurahsState();
+  State<SurahIndex> createState() => _SurahIndexState();
 }
 
-class _SurahsState extends State<Surahs> {
+class _SurahIndexState extends State<SurahIndex> {
   late Map mapresp;
   late List listresp = [];
 
-  Future CallingAPI() async {
+  Future callapi() async {
     http.Response response =
         await http.get(Uri.parse("https://api.alquran.cloud/v1/meta"));
 
-
-        if (response.statusCode == 200) {
-          mapresp = jsonDecode(response.body);
-          listresp = mapresp["data"]["surahs"]["references"];
-        }
+    if (response.statusCode == 200) {
+      setState(() {
+        mapresp = jsonDecode(response.body);
+        listresp = mapresp["data"]["surahs"]["references"];
+      });
+    }
   }
+
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    CallingAPI();
+    callapi();
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: listresp.isNotEmpty ? ListView.builder(
-        itemCount: listresp == null ? 0 : listresp.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            onTap: () {
-              Navigator.push(context, 
-              MaterialPageRoute(
-                builder: (context) => DetailSurah(index + 1)
-                ));
-            },
-            leading: CircleAvatar(
-              backgroundColor: Colors.indigo[900],
-              radius: 30,
-              child: Text(
-                listresp[index]["number"].toString(),
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-            title: Text(listresp[index]["name"] + " | " + listresp[index]["englishName"], style: GoogleFonts.amiriQuran(),),
-            subtitle: Text(listresp[index]["englishNameTranslation"], style: GoogleFonts.amiriQuran(),),
-            trailing: Text("Verses\n" + listresp[index]["numberofAyahs"].toString()),
-
-          );
-        }
-        )
-        : Center(child: CircularProgressIndicator()),
+      body: listresp.isNotEmpty
+            ? ListView.builder(
+                itemCount: listresp == null ? 0 : listresp.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => DetailSurah(index + 1),
+                          ));
+                    },
+                    leading: CircleAvatar(
+                      backgroundColor: Colors.indigo[900],
+                      radius: 30,
+                      child: Text(
+                        listresp[index]["number"].toString(),
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    title: Text(listresp[index]["name"] +
+                        " | " +
+                        listresp[index]["englishName"],style: GoogleFonts.amiriQuran(),),
+                    subtitle: Text(listresp[index]["englishNameTranslation"]),
+                    trailing: Text("Verses\n" +
+                        listresp[index]["numberOfAyahs"].toString()),
+                  );
+                },
+              )
+            : Center(child: CircularProgressIndicator()),
+      
     );
   }
 }
 
 class DetailSurah extends StatefulWidget {
-  const DetailSurah(int i, {super.key});
+  var indexnumber;
+  DetailSurah(this.indexnumber, {super.key});
 
   @override
   State<DetailSurah> createState() => _DetailSurahState();
 }
 
 class _DetailSurahState extends State<DetailSurah> {
+  late Map mapresp;
+  late List listresp = [];
+
+  Future callapi() async {
+    int snum = widget.indexnumber;
+    http.Response response =
+        await http.get(Uri.parse("https://api.alquran.cloud/v1/surah/${snum}"));
+
+    if (response.statusCode == 200) {
+      setState(() {
+        //  responsedata=response.body;
+
+        mapresp = jsonDecode(response.body);
+        listresp = mapresp["data"]["ayahs"];
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    callapi();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: listresp.isNotEmpty
+            ? ListView.builder(
+                itemCount: listresp == null ? 0 : listresp.length,
+                itemBuilder: (context, index) {
+                  return ListTile(
+                    title: Text(listresp[index]["text"], style: GoogleFonts.amiriQuran(),textDirection: TextDirection.rtl,),
+                  );
+                },
+              )
+            : Center(child: CircularProgressIndicator()),
+      
+    );
   }
 }
